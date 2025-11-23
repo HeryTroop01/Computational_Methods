8314a964bde0a236f21e8a3c1fd4c99c8fbf1e0e
HeryTroop01 <punithanvadivel28@icloud.com>
2025-11-23 12:45:53 +0530

    Update Gauss Siedel Iterative Method.py

--- Gauss Siedel Iterative Method.py
+++ Gauss Siedel Iterative Method.py
@@ -1,29 +1,24 @@
-
-
-def gauss_siedel(a, b, x, iters):
+def gauss_seidel(a, b, x, iters):
     n = len(a)
     
     for k in range(iters):
         for i in range(n):
-            s1 = 0
-            s2 = 0
+            s1 = sum(a[i][j] * x[j] for j in range(i))          # old + new values
+            s2 = sum(a[i][j] * x[j] for j in range(i + 1, n))   # old values only
             
-            for j in range(i):
-                s1 += a[i][j] * x[j]
-            for j in range(i):
-                s2 += a[i][j] * x[j] 
-                
-            x[i] = (b[i] - s1 - s2)/a[i][i]
-    return x       
+            x[i] = (b[i] - s1 - s2) / a[i][i]
+    
+    return x
 
+# Input
 a = [
-     [3,5,6],
-     [5, 7,8],
-     [5,6,7]] 
-b= [4,7,8]
-x= [0,0,0]
+     [3, 5, 6],
+     [5, 7, 8],
+     [5, 6, 7]
+]
+b = [4, 7, 8]
+x = [0, 0, 0]
 
 iters = 8
-output = gauss_siedel(a, b, x, iters)       
-print(output)               
-                
+output = gauss_seidel(a, b, x, iters)
+print(output)
