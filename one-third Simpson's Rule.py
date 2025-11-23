358cb8ee5c63f4d97bb144e6b03405c08abdaf71
HeryTroop01 <punithanvadivel28@icloud.com>
2025-11-23 12:48:38 +0530

    Update one-third Simpson's Rule.py

--- one-third Simpson's Rule.py
+++ one-third Simpson's Rule.py
@@ -1,12 +1,11 @@
 def f(x):
     return x**2
 
-
 def simpson(a, b, n):
     h = (b - a) / n
-    x = a
     s = f(a) + f(b)
 
+    # Simpson 1/3 rule (n must be even)
     if n % 2 == 0:
         for i in range(1, n):
             x = a + i*h
@@ -14,8 +13,9 @@ def simpson(a, b, n):
                 s += 2 * f(x)
             else:
                 s += 4 * f(x)
-        return (h/3) * s
+        return (h / 3) * s
 
+    # Simpson 3/8 rule (n must be divisible by 3)
     elif n % 3 == 0:
         for i in range(1, n):
             x = a + i*h
@@ -23,15 +23,15 @@ def simpson(a, b, n):
                 s += 2 * f(x)
             else:
                 s += 3 * f(x)
-        return (3*h/8) * s
+        return (3 * h / 8) * s
 
     else:
-        return None
+        return None   # invalid n value for Simpson's rules
 
-
+# Inputs
 a = 0
 b = 4
-n = 4    
+n = 4
 
 answer = simpson(a, b, n)
 print(answer)
