1ca9212a3da66f04f5f956da27b6fa8807490f30
HeryTroop01 <punithanvadivel28@icloud.com>
2025-11-23 12:56:00 +0530

    Update Complete_Pivoting.py

--- Complete_Pivoting.py
+++ Complete_Pivoting.py
@@ -1,46 +1,53 @@
 import numpy as np
 
 def complete_pivot(A, b):
-    n = len(A)
     A = A.astype(float)
     b = b.astype(float)
-    col_index = np.arange(n) 
+    n = len(A)
+
+    # To keep track of column swaps
+    col_index = np.arange(n)
 
     for k in range(n - 1):
-        sub = np.abs(A[k:, k:])
-        i_max, j_max = np.unravel_index(np.argmax(sub), sub.shape)
-        i_max += k
-        j_max += k
 
+        # Find pivot (max element) in the submatrix A[k:, k:]
+        i, j = np.unravel_index(np.argmax(abs(A[k:, k:])), (n - k, n - k))
+        i += k
+        j += k
 
-        if i_max != k:
-            A[[k, i_max]] = A[[i_max, k]]
-            b[[k, i_max]] = b[[i_max, k]]
+        # Swap rows
+        if i != k:
+            A[[k, i]] = A[[i, k]]
+            b[[k, i]] = b[[i, k]]
 
-        if j_max != k:
-            A[:, [k, j_max]] = A[:, [j_max, k]]
-            col_index[[k, j_max]] = col_index[[j_max, k]]
+        # Swap columns
+        if j != k:
+            A[:, [k, j]] = A[:, [j, k]]
+            col_index[[k, j]] = col_index[[j, k]]
 
-        for i in range(k + 1, n):
-            f= A[i, k] / A[k, k]
-            A[i, k:] -= f* A[k, k:]
-            b[i] -= f * b[k]
+        # Elimination
+        for r in range(k + 1, n):
+            factor = A[r, k] / A[k, k]
+            A[r, k:] -= factor * A[k, k:]
+            b[r] -= factor * b[k]
 
+    # Back substitution
     x = np.zeros(n)
     for i in range(n - 1, -1, -1):
-        x[i] = (b[i] - np.dot(A[i, i + 1:], x[i + 1:])) / A[i, i]
+        x[i] = (b[i] - A[i, i + 1:] @ x[i + 1:]) / A[i, i]
 
-    y = np.zeros_like(x)
-    for i in range(n):
-        y[col_index[i]] = x[i]
+    # Reorder solution according to swapped columns
+    final_x = np.zeros(n)
+    final_x[col_index] = x
 
-    return y
+    return final_x
 
+
+# Example
 A = np.array([[6, 2, 3],
               [1, 3, 4],
               [2, 1, 3]])
+
 b = np.array([5, 14, 13])
 
-
-x = complete_pivot(A.copy(), b.copy())
-print(x)
+print(complete_pivot(A, b))
