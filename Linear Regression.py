36fbeb1515d5b5d591bfff249a9cdaad66794611
HeryTroop01 <punithanvadivel28@icloud.com>
2025-11-23 12:46:42 +0530

    Update Linear Regression.py

--- Linear Regression.py
+++ Linear Regression.py
@@ -1,15 +1,19 @@
-x = [1,2,3,4]
-y = [2,3,5,4]
+# Data points
+x = [1, 2, 3, 4]
+y = [2, 3, 5, 4]
 
 n = len(x)
 
+# Summations
 sum_x = sum(x)
 sum_y = sum(y)
-sum_x2 = sum([i*i for i in x])
-sum_xy = sum([x[i] * y[i] for i in range(n)])
+sum_x2 = sum(i * i for i in x)
+sum_xy = sum(x[i] * y[i] for i in range(n))
 
-b = (n*sum_xy - sum_x*sum_y) / (n*sum_x2 - sum_x*sum_x)
-a = (sum_y - b*sum_x) / n
+# Regression coefficients
+b = (n * sum_xy - sum_x * sum_y) / (n * sum_x2 - sum_x ** 2)
+a = (sum_y - b * sum_x) / n
 
+# Output
 print("a =", a)
 print("b =", b)
